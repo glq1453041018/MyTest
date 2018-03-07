a65d295b8f89ff2b4522897ae53e25fd6c323392
glq <1453041018@qq.com>
2018-03-07 14:14:36 +0800

    ceshi
    测试

--- iOS/SuperScholar/SuperScholar/AdsModule/IndexModule/AdsViewController.m
+++ iOS/SuperScholar/SuperScholar/AdsModule/IndexModule/AdsViewController.m
@@ -16,7 +16,10 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    
+    
+    
+    //
 }
 
 - (void)didReceiveMemoryWarning {
