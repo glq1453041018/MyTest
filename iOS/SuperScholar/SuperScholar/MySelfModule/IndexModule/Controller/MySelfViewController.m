a58fbf104bfd33ec45c0fc637b0f34e891077ee1
chenweinan <1014949353@qq.com>
2018-03-16 20:08:48 +0800

    修改个人中心ui

--- iOS/SuperScholar/SuperScholar/MySelfModule/IndexModule/Controller/MySelfViewController.m
+++ iOS/SuperScholar/SuperScholar/MySelfModule/IndexModule/Controller/MySelfViewController.m
@@ -9,10 +9,15 @@
 #import "MySelfViewController.h"
 #import "MYAutoScaleView.h"
 #import "LoginInterfaceViewController.h"
+#import "UIImage+ImageEffects.h"
 
 @interface MySelfViewController ()<UITableViewDataSource, UITableViewDelegate>
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
-@property (weak, nonatomic) IBOutlet UIView *topView;
+@property (weak, nonatomic) IBOutlet UIView *tabelHeaderView;//列表header
+@property (weak, nonatomic) IBOutlet MYAutoScaleView*topBackView;//头部背景视图
+@property (weak, nonatomic) IBOutlet UIView *topView;//头部视图
+
+@property (strong, nonatomic) NSMutableArray *data;//数据
 @end
 
 @implementation MySelfViewController
@@ -34,7 +39,8 @@
     self.didLoginView.hidden = GetInfoForKey(UserId_NSUserDefaults) == nil;
     self.loginButtton.hidden = !self.didLoginView.hidden;
     self.navigationBar.hidden = self.loginButtton.hidden;
-    self.backImageView.image = [UIImage imageNamed:@"timg.jpg"];
+    UIImage *image = [UIImage imageNamed:@"testimage"];
+    self.backImageView.image = [image applyBlurWithRadius:5 tintColor:[KColorTheme colorWithAlphaComponent:0.1] saturationDeltaFactor:1.0 maskImage:nil];
 }
 
 
@@ -42,7 +48,8 @@
 #pragma mark - <************************** 获取数据 **************************>
 // !!!: 获取数据
 -(void)getDataFormServer{
-    
+    self.data = [NSMutableArray arrayWithObjects:@[@"消息通知", @"我的动态"],  @[@"用户反馈", @"当前版本"], nil];
+    [self.tableView reloadData];
 }
 
 
@@ -61,48 +68,89 @@
 - (void)configTableView{
     self.tableView.backgroundColor = [UIColor groupTableViewBackgroundColor];
     self.tableView.showsVerticalScrollIndicator = NO;
-    self.tableView.tableFooterView = [[UIView alloc] init];
+    self.tableView.tableFooterView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 0, CGFLOAT_MIN)];
     self.automaticallyAdjustsScrollViewInsets = NO;
-    self.tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
-    
-    MYAutoScaleView *scale = [[MYAutoScaleView alloc] initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, 175)];
-    [scale setContentView:self.topView scrollview:self.tableView];
-    self.tableView.tableHeaderView = scale;
+//    self.tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
+
+    [self.topBackView setContentView:self.topView scrollview:self.tableView];
+    self.tabelHeaderView.viewHeight = 245;
+    self.tableView.tableHeaderView = self.tabelHeaderView;
 }
 
 
 #pragma mark - <*********************** 初始化控件/数据 **********************>
-
+- (NSMutableArray *)data{
+    if(!_data){
+        _data = [NSMutableArray array];
+    }
+    return _data;
+}
 
 
 
 #pragma mark - <************************** 代理方法 **************************>
 #pragma mark UITableViewDataSource
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
-    return 1;
+    return [self.data count];
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
-    return 10;
+    return [[self.data objectAtIndex:section] count];
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    NSString *text = [[self.data objectAtIndex:indexPath.section] objectAtIndex:indexPath.row];
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cellid"];
     if(cell == nil){
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"cellid"];
+        cell.textLabel.font = [UIFont systemFontOfSize:FontSize_16];
+        cell.selectionStyle = UITableViewCellSelectionStyleNone;
     }
-    [cell.textLabel setText:[NSString stringWithFormat:@"这是第%ld行", indexPath.row]];
+    [cell.textLabel setText:text];
     return cell;
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
-    return 50;
+    return 44;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section{
+    return 10;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section{
+    return section == 0 ? 10 : CGFLOAT_MIN;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    NSString *text = [[self.data objectAtIndex:indexPath.section] objectAtIndex:indexPath.row];
+    NSString *tip = [NSString stringWithFormat:@"跳转%@", text];
+    LLAlert(tip);
 }
 
 #pragma mark - <************************** 点击事件 **************************>
 - (IBAction)onClickMSMBtn:(UIButton *)sender {
     LoginInterfaceViewController *loginVc = [LoginInterfaceViewController new];
     [self presentViewController:loginVc animated:YES completion:nil];
+}
+- (IBAction)onClickTitleButtons:(UIControl *)sender {
+    // !!!: 评论、收藏、历史点击事件
+    switch (sender.tag) {
+        case 0://评论
+            LLAlert(@"跳转评论");
+            break;
+        case 1://收藏
+            LLAlert(@"跳转收藏");
+            break;
+        case 2://历史
+            LLAlert(@"跳转历史");
+            break;
+        default:
+            break;
+    }
+}
+- (IBAction)onClickHeaderImage:(UIButton *)sender {
+    LLAlert(@"跳转个人资料");
 }
 
 
---
+++ iOS/SuperScholar/SuperScholar/Public/View/LLAlertView/LLAlertView.h
@@ -0,0 +1,74 @@
+//
+//  LLAlertView.h
+//  GuDaShi
+//
+//  Created by LOLITA on 2017/11/30.
+//  Copyright © 2017年 songzhaojie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+typedef void(^LLAlertViewBlock)(NSInteger index);
+
+@interface LLAlertView : UIView
+@property (copy ,nonatomic) LLAlertViewBlock block;
+
+@property (strong, nonatomic) UIView *backgroundView;
+
+@property (assign ,nonatomic) BOOL isShow;
+
+/**
+ 是否需要关闭按钮，默认不需要
+ */
+@property (assign ,nonatomic) BOOL needCloseBtn;
+
+/**
+ 是否点击背景关闭，默认不需要
+ */
+@property (assign ,nonatomic) BOOL touchToClose;
+
+/**
+ 传入自定义视图
+ */
+@property (strong, nonatomic) UIView *contentView;
+/**
+ 传入自定义动画
+ */
+@property (strong ,nonatomic) CAAnimation *animation;
+
+/**
+ 点击背景视图回调
+ */
+@property (nonatomic,copy) void(^touchBgView)();
+
+
+/**
+ 显示
+ */
+-(void)show;
+
+/**
+ 隐藏
+ */
+-(void)hide;
+
+/**
+ 隐藏
+ */
+-(void)hideWithBlock:(void(^)())block;
+
+
+/**
+ 显示系统弹窗AlertViewController
+ */
++(void)showSystemAlertViewClickBlock:(LLAlertViewBlock)block message:(NSString*)message buttonTitles:(NSString *)buttonTitles, ... NS_REQUIRES_NIL_TERMINATION;
+
+/**
+ 显示系统弹窗AlertView , title 默认 “提示”
+ */
+-(void)showSystemAlertViewClickBlock:(LLAlertViewBlock)block message:(NSString*)message buttonTitles:(NSString *)buttonTitles, ... NS_REQUIRES_NIL_TERMINATION;
+
+/**
+ 显示系统弹窗AlertView ,  参数title 
+ */
+-(UIAlertView *)showSystemAlertViewClickBlock:(LLAlertViewBlock)block AlertViewTitle:(NSString *)title message:(NSString *)message buttonTitles:(NSString *)buttonTitles, ... NS_REQUIRES_NIL_TERMINATION;
+@end
