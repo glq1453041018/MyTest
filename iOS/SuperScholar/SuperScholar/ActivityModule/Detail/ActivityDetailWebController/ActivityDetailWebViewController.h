50efb85fee60dc8acf24844b898843ed28fa8444
LOLITA0164 <476512340@qq.com>
2018-03-23 17:34:38 +0800

    分享图片

---
+++ iOS/SuperScholar/SuperScholar/ActivityModule/Detail/ActivityDetailWebController/ActivityDetailWebViewController.h
@@ -0,0 +1,14 @@
+//
+//  ActivityDetailWebViewController.h
+//  SuperScholar
+//
+//  Created by 伟南 陈 on 2018/3/23.
+//  Copyright © 2018年 SuperScholar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "MyWebView.h"
+
+@interface ActivityDetailWebViewController : UIViewController
+@property (strong, nonatomic) IBOutlet MyWebView *webView;
+@end
